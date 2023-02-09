57d8603390f4f713eea31550c0c30c320b3726a9
Dang Thanh <115873719+DangThanh777@users.noreply.github.com>
2023-02-09 22:16:25 +0700

    Add files via upload

---
+++ JavaExercise.java
@@ -0,0 +1,13 @@
+package fa.training.assignment1;
+
+public class JavaExercise {
+
+	public static void main(String[] args) {
+		System.out.printf(" J    a   v     v  a \n"
+				+ " J   a a   v   v  a a \n"
+				+ "J  J  aaaaa   V V  aaaaa \n"
+				+ "JJ  a     a   V  a     a" );
+
+	}
+
+}
