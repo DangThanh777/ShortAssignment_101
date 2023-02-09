54061e35928585f7d2cfd40d0528119ec7a631d3
Dang Thanh <115873719+DangThanh777@users.noreply.github.com>
2023-02-09 22:28:14 +0700

    Add files via upload

---
+++ CircleExercise.java
@@ -0,0 +1,13 @@
+package fa.training.assignment1;
+
+public class CircleExercise {
+
+	public static void main(String[] args) {
+		float radius = 7.5f;
+		
+		System.out.println("Perimeter is = " + (radius * 2 * 3.14));
+		System.out.println("Area is = " + (radius * radius * 3.14));
+
+	}
+
+}
---
+++ RectangleExercise.java
@@ -0,0 +1,14 @@
+package fa.training.assignment1;
+
+public class RectangleExercise {
+
+	public static void main(String[] args) {
+		float width = 5.5f;
+		float height = 8.5f;
+
+		System.out.println("Area is " + width + " * " + height + " = " + (width * height));
+		System.out.println("Perimeter is 2 * " + "(" + width + " + " + height + ")" + " = " + ( 2 *(width + height)));
+
+	}
+
+}
