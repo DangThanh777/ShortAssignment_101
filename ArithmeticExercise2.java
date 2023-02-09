db71d8df53296beb7617752767c6b106a1804416
Dang Thanh <115873719+DangThanh777@users.noreply.github.com>
2023-02-09 22:11:48 +0700

    Add files via upload

---
+++ ArithmeticExercise2.java
@@ -0,0 +1,17 @@
+package fa.training.assignment1;
+
+public class ArithmeticExercise2 {
+
+	public static void main(String[] args) {
+		int a = 125;
+		int b = 24;
+
+		System.out.println(a + " + " + b + " = " + (a + b));
+		System.out.println(a + " - " + b + " = " + (a - b));
+		System.out.println(a + " * " + b + " = " + (a * b));
+		System.out.println(a + " / " + b + " = " + (a / b));
+		System.out.println(a + " % " + b + " = " + (a % b));
+
+	}
+
+}
